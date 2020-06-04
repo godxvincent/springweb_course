1d94bb101df69668cbec7743617f200d70d6fc75
Ricardo_Vargas <ricardo.vargas@avaldigitallabs.com>
2020-06-04 13:17:56 -0500

    Este es un commit de prueba de pull request

--- src/main/java/com/godxvincent/springweb/SpringwebApplication.java
+++ src/main/java/com/godxvincent/springweb/SpringwebApplication.java
@@ -3,6 +3,7 @@ package com.godxvincent.springweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Esto es un comentario de prueba 
 @SpringBootApplication
 public class SpringwebApplication {
 
